de4449e09874681d26fcce15463d55ef0b8929c3
Cona7 <jedembalce@gmail.com>
2019-10-01 12:19:42 +0200

    Search screen UI

---
+++ GitHubSearch/Managers/ColorManager.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+// Color palette
+
+extension UIColor {
+
+    // swiftlint:disable object_literal
+    @nonobjc class var light: UIColor {
+        return UIColor(red: 255 / 255.0, green: 255 / 255.0, blue: 255 / 255.0, alpha: 1.0)
+    }
+
+    @nonobjc class var dark: UIColor {
+        return UIColor(red: 0 / 255.0, green: 0 / 255.0, blue: 0 / 255.0, alpha: 1.0)
+    }
+}
--- GitHubSearch/Modules/SearchViewController.swift
+++ GitHubSearch/Modules/SearchViewController.swift
@@ -1,14 +1,58 @@
 import UIKit
+import SnapKit
+import SimpleDataSource
 
 final class SearchViewController: ViewController {
     var presenter: SearchPresenterInterface!
 
+    var tableViewDataSource = SimpleTableViewDataSource()
+
+    lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+
+        imageView.image = #imageLiteral(resourceName: "GitHub-Mark")
+
+        return imageView
+    }()
+
+    lazy var label: UILabel = {
+        let label = UILabel()
+
+        label.font = .boldFont(size: 36)
+        label.textColor = .darkText
+        label.text = "Search GitHub"
+
+        return label
+    }()
+
+    lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar(frame: .zero)
+
+        searchBar.searchBarStyle = .minimal
+        searchBar.backgroundColor = .clear
+        searchBar.barTintColor = .clear
+        searchBar.placeholder = "Users and Repos"
+
+        return searchBar
+    }()
+
+    lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: CGRect.zero)
+
+        tableView.delegate = self.tableViewDataSource
+        tableView.dataSource = self.tableViewDataSource
+        tableView.backgroundColor = .light
+        tableView.separatorStyle = .none
+
+        return tableView
+    }()
+
     override func setupViewController() {
         super.setupViewController()
 
-        self.view.backgroundColor = .white
+        navigationController?.isNavigationBarHidden = true
 
-        title = "Search"
+        self.view.backgroundColor = .white
 
         presenter
             .viewModelDriver
@@ -16,11 +60,44 @@ final class SearchViewController: ViewController {
                 onNext: { [weak self] viewModel in
                     self?.present(viewModel: viewModel)
                 }
-            ).disposed(by: disposeBag)
+        ).disposed(by: disposeBag)
+    }
+
+    override func addSubviews() {
+        view.addSubview(imageView)
+        view.addSubview(label)
+        view.addSubview(searchBar)
+        view.addSubview(tableView)
+    }
+
+    override func layout() {
+        imageView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(32)
+            make.centerX.equalToSuperview()
+            make.size.equalTo(160)
+        }
+
+        label.snp.makeConstraints { make in
+            make.top.equalTo(imageView.snp.bottom)
+            make.left.right.equalToSuperview().inset(UIEdgeInsets(top: 120, left: 24, bottom: 0, right: 24))
+        }
+
+        searchBar.snp.makeConstraints { make in
+            make.left.right.equalToSuperview().inset(UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16))
+            make.top.equalTo(label.snp.bottom).offset(8)
+            make.height.equalTo(32)
+        }
+
+        tableView.snp.makeConstraints { make in
+            make.left.right.bottom.equalToSuperview()
+            make.top.equalTo(searchBar.snp.bottom)
+        }
     }
 }
 
 extension SearchViewController {
     func present(viewModel: SearchViewModel) {
+
+        tableViewDataSource.present(viewModels: viewModel.cellViewModels, onTableView: self.tableView)
     }
 }
